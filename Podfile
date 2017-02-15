796ed89bb2be63e2da0bc714c2df087411d03391
Jorge Osuna Benitez <Jorge@Jorges-MacBook-Air.local>
2017-02-15 09:40:48 -0800

    added alamofire

---
+++ Podfile
@@ -0,0 +1,9 @@
+# Uncomment the next line to define a global platform for your project
+source 'https://github.com/Cocoapods/Specs.git'
+# platform :ios, '9.0'
+use_frameworks!
+
+target 'pokedex3' do
+pod 'Alamofire', '~> 4.0'
+end
+
--- PokemonDetailVC.swift
+++ PokemonDetailVC.swift
@@ -10,26 +10,36 @@ import UIKit
 
 class PokemonDetailVC: UIViewController {
 
+    var pokemon: Pokemon!
+    
+    
+    
+    
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
+        nameLbl.text = pokemon.name
+        
+    }
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
